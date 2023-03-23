83a58f53d744a8e85b92cc21aa09cac2014183e9
Pablo Marín <pablomaringallardo17@gmail.com>
2023-03-23 17:06:15 +0100

    feat: creo la clase CryptoForm y los campos del formulario en forms.py

--- balance/forms.py
+++ balance/forms.py
@@ -3,3 +3,13 @@ from flask_wtf import FlaskForm
 from wtforms import DateField, FloatField, IntegerField, RadioField, StringField, SubmitField
 from wtforms.widgets import HiddenInput
 from wtforms.validators import DataRequired, Regexp
+
+
+class CryptoForm(FlaskForm):
+    id = IntegerField(default = 0, widget=HiddenInput())
+    monedaFrom = StringField('Moneda origen', validators=[DataRequired(message='Campo obligatorio.')])
+    cantidadFrom = FloatField('Cantidad a invertir', validators=[DataRequired(message='Campo obligatorio.')])
+    monedaTo = StringField('Moneda destino', validators=[DataRequired(message='Campo obligatorio.')])
+
+    botonCalcular = SubmitField('Calcular')
+    botonEnviar = SubmitField('Enviar')
