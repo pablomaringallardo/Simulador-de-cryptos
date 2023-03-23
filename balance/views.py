28a168f0c497d8ba7bfd4fa217ddce2edf7a6274
Pablo Marín <pablomaringallardo17@gmail.com>
2023-03-23 16:47:28 +0100

    fix: creo un endpoint para consultar las inversiones (de la BBDD) y se las envio a JS para que las pinte  en la pagina de inicio incluido el precio unitario

--- balance/views.py
+++ balance/views.py
@@ -1,5 +1,4 @@
-from flask import render_template
-import json
+from flask import jsonify, render_template
 import requests
 from . import app, APIKEY
 from .models import APIConnect, DBConnect
@@ -9,18 +8,7 @@ RUTA = app.config.get('RUTA')
 
 @app.route('/')
 def home():
-    db = DBConnect(RUTA)
-    mostrar_inversiones = db.mostrarInversiones()
-    for inv in mostrar_inversiones:
-        monedaFrom = inv.get('monedaFrom')
-        monedaTo = inv.get('monedaTo')
-        conexionApi = APIConnect()
-        precioUnitario = conexionApi.consultar_cambio(monedaFrom, monedaTo)
-        # inv['precioUnidad'] = precioUnitario
-        # precioAñadido = json.dump
-    
-    return render_template('inicio.html', inv=mostrar_inversiones) #inv=precioAñadido)
-
+    return render_template('inicio.html')
 
 @app.route('/purchase')
 def formulario():
@@ -29,3 +17,42 @@ def formulario():
 @app.route('/status')
 def status():
     return render_template('status.html')
+
+@app.route('/api/v1/inversiones')
+def mostrar_inversiones():
+    try:
+        db = DBConnect(RUTA)
+        consulta = 'SELECT date, time, monedaFrom, cantidadFrom, monedaTo, cantidadTo FROM Movimientos'
+        inversiones = db.consultaSQL(consulta)
+        print('1', inversiones)
+        inversiones_con_PU = []
+        for inv in inversiones:
+            cantidadFrom = inv['cantidadFrom']
+            cantidadTo = inv['cantidadTo']
+            precioUnitario = cantidadFrom / cantidadTo
+            print('4', precioUnitario)
+            inv['rate'] = precioUnitario
+            print(inv)
+            inversiones_con_PU.append(inv)
+
+        if inversiones_con_PU:
+            resultado = {
+                'status': 'success',
+                'results': inversiones_con_PU
+            }
+            status_code = 200
+        else:
+            resultado = {
+                'status': 'error',
+                'message': 'No hay inversiones en la base de datos'
+            }
+            status_code = 404
+
+    except:
+        resultado = {
+            'status': 'error',
+            'message': 'Error interno del sistema.'
+        }
+        status_code = 500
+    
+    return jsonify(resultado), status_code
