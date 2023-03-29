9018438294ea30e5c597003765f2dbf9226d00c4
Pablo Marín <pablomaringallardo17@gmail.com>
2023-03-29 17:47:39 +0200

    feat: creo endpoint para saber el status (total € invertidos y saldo € invertidos) WP

--- balance/views.py
+++ balance/views.py
@@ -1,6 +1,7 @@
 from flask import jsonify, render_template
 import requests
 from . import app, APIKEY
+from .forms import CryptoForm
 from .models import APIConnect, DBConnect
 
 RUTA = app.config.get('RUTA')
@@ -12,7 +13,8 @@ def home():
 
 @app.route('/purchase')
 def formulario():
-    return render_template('form.html')
+    formulario = CryptoForm()
+    return render_template('form.html', form=formulario)
 
 @app.route('/status')
 def status():
@@ -24,15 +26,12 @@ def mostrar_inversiones():
         db = DBConnect(RUTA)
         consulta = 'SELECT date, time, monedaFrom, cantidadFrom, monedaTo, cantidadTo FROM Movimientos'
         inversiones = db.consultaSQL(consulta)
-        print('1', inversiones)
         inversiones_con_PU = []
         for inv in inversiones:
             cantidadFrom = inv['cantidadFrom']
             cantidadTo = inv['cantidadTo']
             precioUnitario = cantidadFrom / cantidadTo
-            print('4', precioUnitario)
             inv['rate'] = precioUnitario
-            print(inv)
             inversiones_con_PU.append(inv)
 
         if inversiones_con_PU:
@@ -55,4 +54,46 @@ def mostrar_inversiones():
         }
         status_code = 500
     
+    return jsonify(resultado), status_code
+
+@app.route('/api/v1/estado')
+def mostrar_estado():
+    try:
+        db = DBConnect(RUTA)
+        consulta = 'SELECT date, time, monedaFrom, cantidadFrom, monedaTo, cantidadTo FROM Movimientos'
+        inversiones = db.consultaSQL(consulta)
+        print('0', inversiones)
+
+        total_euros_invertidos = 0
+        euros_atrapados = 0
+        for inv in inversiones:
+            if inv['monedaFrom'] == 'EUR':
+                total_euros_invertidos += inv['cantidadFrom']
+        for inv in inversiones:
+            if inv['monedaTo'] == 'EUR':
+                euros_atrapados += inv['cantidadTo']
+        
+        saldo_euros_invertidos = euros_atrapados - total_euros_invertidos
+        estados = {}
+        estados['total_euros_invertidos'] = total_euros_invertidos
+        estados['saldo_euros_invertidos'] = saldo_euros_invertidos
+
+        if estados:
+            resultado = {
+                'status': 'success',
+                'results': estados
+            }
+            status_code = 200
+        else:
+            resultado = {
+                'status': 'error',
+                'message': 'Error'
+            }
+            status_code = 404
+    except:
+        resultado = {
+            'status': 'error',
+            'message': 'Error interno del servidor.'
+        }
+
     return jsonify(resultado), status_code
