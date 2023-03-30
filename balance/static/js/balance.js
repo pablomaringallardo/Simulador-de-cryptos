a6612fb7e4f136c2d9748b8eaa8704459539b154
Pablo Marín <pablomaringallardo17@gmail.com>
2023-03-30 18:07:31 +0200

    feat: cambios pequeños

--- balance/static/js/balance.js
+++ balance/static/js/balance.js
@@ -1,13 +1,11 @@
-const peticion = new XMLHttpRequest()
+const peticion = new XMLHttpRequest();
 
 function cargarInversiones() {
-    console.log('Entro a cargarInversiones')
     peticion.open('GET', 'http://127.0.0.1:5000/api/v1/inversiones', true);
     peticion.send();
 };
 
 function mostrarInversiones() {
-    console.log('entro a mostrarInversiones')
     if (this.readyState === 4 && this.status === 200) {
         const response = JSON.parse(peticion.responseText);
         const inversiones = response.results;
