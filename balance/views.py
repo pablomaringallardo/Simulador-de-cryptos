0d0700950ffc33f6a3baa62122793c8b5abf09fb
Pablo Marín <pablomaringallardo17@gmail.com>
2023-03-30 17:43:06 +0200

    feat: modifico endpoint para enviar al javascript la cantidad de monedas que tengo

--- balance/views.py
+++ balance/views.py
@@ -60,23 +60,66 @@ def mostrar_inversiones():
 def mostrar_estado():
     try:
         db = DBConnect(RUTA)
-        consulta = 'SELECT date, time, monedaFrom, cantidadFrom, monedaTo, cantidadTo FROM Movimientos'
+        consulta = 'SELECT monedaFrom, cantidadFrom, monedaTo, cantidadTo FROM Movimientos'
         inversiones = db.consultaSQL(consulta)
-        print('0', inversiones)
 
+        # Total de € invertidos
         total_euros_invertidos = 0
         euros_atrapados = 0
         for inv in inversiones:
             if inv['monedaFrom'] == 'EUR':
                 total_euros_invertidos += inv['cantidadFrom']
-        for inv in inversiones:
             if inv['monedaTo'] == 'EUR':
                 euros_atrapados += inv['cantidadTo']
         
+        # Saldo de euros invertidos
         saldo_euros_invertidos = euros_atrapados - total_euros_invertidos
+
+        # Valor actual
+        cryptos = ['BTC', 'ETH', 'USDT', 'ADA', 'SOL', 'XRP', 'DOT', 'DOGE', 'SHIB']
+
+        valor_actual = 0
+        for crypto in cryptos:
+            for inv in inversiones:
+                suma_cantidadTo = 0
+                suma_cantidadFrom = 0
+                if inv['monedaTo'] == crypto:
+                    suma_cantidadTo += inv['cantidadTo']
+                elif inv['monedaFrom'] == crypto:
+                    suma_cantidadFrom += inv['cantidadFrom']
+                
+                total_crypto = suma_cantidadTo - suma_cantidadFrom
+
+                if inv['monedaTo'] == crypto or inv['monedaFrom'] == crypto:
+                    conexionAPI = APIConnect()
+                    consultar_cambio = conexionAPI.consultar_cambio(crypto, 'EUR')
+
+                cambio = consultar_cambio * total_crypto
+
+                valor_actual += cambio
+        
         estados = {}
         estados['total_euros_invertidos'] = total_euros_invertidos
-        estados['saldo_euros_invertidos'] = saldo_euros_invertidos
+        estados['valor_total_cryptos'] = valor_actual
+        estados['valor_actual'] =  saldo_euros_invertidos + total_euros_invertidos + valor_actual
+        # Cryptos en posesión
+        
+        for crypto in cryptos:
+            moneda_obtenida = 0
+            moneda_invertida = 0
+            for inv in inversiones:
+                if inv['monedaTo'] == crypto:
+                    print(inv['monedaTo'])
+                    moneda_obtenida += inv['cantidadTo']
+                    print(inv['cantidadTo'])
+                    print(moneda_obtenida)
+                if inv['monedaFrom'] == crypto:
+                    moneda_invertida += inv['cantidadFrom']
+                    
+            total_moneda = moneda_obtenida - moneda_invertida
+            estados[crypto] = total_moneda
+
+
 
         if estados:
             resultado = {
@@ -95,5 +138,6 @@ def mostrar_estado():
             'status': 'error',
             'message': 'Error interno del servidor.'
         }
+        status_code = 500
 
     return jsonify(resultado), status_code
