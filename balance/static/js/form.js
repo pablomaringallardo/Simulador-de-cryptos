bc90d9b47efd78386986d222f0e53bce76499f3e
Pablo Marín <pablomaringallardo17@gmail.com>
2023-03-31 21:26:42 +0200

    feat: WP JS del formulario

--- balance/static/js/form.js
+++ balance/static/js/form.js
@@ -1,1 +1,10 @@
-const form = document.getElementById('form');
+const form = document.getElementById("form");
+const botonEnviar = document.getElementById("boton-enviar");
+const botonCalcular = document.getElementById("boton-calcular");
+botonEnviar.addEventListener("submit", sendForm);
+botonCalcular.addEventListener("submit", sendFormCalculate);
+
+function sendForm(event) {
+    alert('Formulario enviado')
+
+}
