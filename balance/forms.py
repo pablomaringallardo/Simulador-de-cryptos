b6d7409ac4d4cd3db4c70b48ab308b896e49aa55
Pablo Marín <pablomaringallardo17@gmail.com>
2023-03-31 21:22:15 +0200

    change: el tipo de campo para los campos monedaFrom y monedaTo

--- balance/forms.py
+++ balance/forms.py
@@ -1,14 +1,23 @@
 from flask_wtf import FlaskForm
-from wtforms import FloatField, IntegerField, StringField, SubmitField
+from wtforms import FloatField, IntegerField, SelectField, SubmitField
 from wtforms.widgets import HiddenInput
 from wtforms.validators import DataRequired
 
 
 class CryptoForm(FlaskForm):
     id = IntegerField(default = 0, widget=HiddenInput())
-    monedaFrom = StringField('Moneda origen', validators=[DataRequired(message='Campo obligatorio.')])
+    
+    monedaFrom = SelectField('Moneda origen',
+                            choices=[('EUR', 'EUR'), ('BTC', 'BTC'), ('ETH', 'ETH'), ('USDT', 'USDT'), ('ADA', 'ADA'),
+                                    ('SOL', 'SOL'), ('XRP', 'XRP'), ('DOT', 'DOT'), ('DOGE', 'DOGE'), ('SHIB', 'SHIB')],
+                            validators=[DataRequired(message='Campo obligatorio.')])
+    
     cantidadFrom = FloatField('Cantidad a invertir', validators=[DataRequired(message='Campo obligatorio.')])
-    monedaTo = StringField('Moneda destino', validators=[DataRequired(message='Campo obligatorio.')])
+
+    monedaTo = SelectField('Moneda destino',
+                            choices=[('EUR', 'EUR'), ('BTC', 'BTC'), ('ETH', 'ETH'), ('USDT', 'USDT'), ('ADA', 'ADA'),
+                                    ('SOL', 'SOL'), ('XRP', 'XRP'), ('DOT', 'DOT'), ('DOGE', 'DOGE'), ('SHIB', 'SHIB')],
+                            validators=[DataRequired(message='Campo obligatorio.')])
 
     botonCalcular = SubmitField('Calcular')
-    botonEnviar = SubmitField('Enviar')
+    botonEnviar = SubmitField('Aceptar')
